7b96fa2644172193c350ec1bad88f488d9b2d12c
Eric Huang <miteric@gmail.com>
2020-03-26 09:39:56 +0800

    refs #236 MyNavibar component

--- vue-router-sample/src/App.vue
+++ vue-router-sample/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <MyNavibar />
     <router-view :key="$route.path" />
   </div>
 </template>
+<script>
+import MyNavibar from "@/components/MyNavibar";
+
+export default {
+  components: {
+    MyNavibar
+  }
+};
+</script>
 
 <style>
 #app {
---
+++ vue-router-sample/src/components/MyNavibar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav id="nav">
+    <p class="logo">HELLO APP</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li class="links">
+        <router-link to="/about">About</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+#nav {
+  display: flex;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.nav-links {
+  display: flex;
+}
+
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+
+.links:hover {
+  text-decoration: underline;
+}
+
+.logo {
+  font-size: 1.2rem;
+  font-weight: 700;
+}
+</style>
